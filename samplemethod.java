6bdd1ccdeff1bc2a44b3ad584665f673128559c0
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-09-21 13:20:04 +0530

    Add files via upload

---
+++ samplemethod.java
@@ -0,0 +1,15 @@
+class Hari
+ {
+    public void getstudent()
+	{
+	   System.out.println("class Student");
+	}
+ }
+
+class samplemethod
+ {   
+    public static void main(String asd[])
+	{
+	  new Hari().getstudent();
+	}
+ }
