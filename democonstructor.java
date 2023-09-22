7b4d740e657bcad2fc0bdc45d6e4ff7428ea2982
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-09-22 12:15:45 +0530

    Add files via upload

---
+++ democonstructor.java
@@ -0,0 +1,18 @@
+class democons1
+ {
+   democons1()
+	{
+	    System.out.println("constructor");
+	} 
+ }
+
+
+class democonstructor
+ {
+    public static void main(String arg[])
+	{
+	   democons1 obj=new democons1();
+           
+        }
+ }
+
