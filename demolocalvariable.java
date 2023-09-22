02aaf1f9dd299b1608837cd6665d0e0c1c2bd9ce
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-09-22 12:39:07 +0530

    Add files via upload

---
+++ demolocalvariable.java
@@ -0,0 +1,18 @@
+class sample
+ {
+   sample(String actor,String movie,String song)
+	{
+	  System.out.println(actor);
+	  System.out.println(movie);  
+	  System.out.println(song);
+	}
+ }
+
+class demolocalvariable
+ {
+   public static void main(String asd[])
+	{
+	   sample obj=new sample("vijay","leo","anna ready poster adi");
+	}
+ }
+
