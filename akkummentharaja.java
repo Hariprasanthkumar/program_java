ddb3464fec4091aeaf296c0f75996ca13c65d312
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-09-29 13:17:47 +0530

    Add files via upload

---
+++ akkummentharaja.java
@@ -0,0 +1,22 @@
+import hari.demopackage;
+
+class viji extends demopackage
+{
+    viji()
+{
+ super();
+
+}
+}
+
+
+
+
+
+public class akkummentharaja {
+   public static void main(String asd[])
+   {
+      demopackage o= new demopackage();
+      o.praveen();
+   }  
+}
