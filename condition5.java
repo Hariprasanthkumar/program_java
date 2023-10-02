6e64225ad33416303be70b0869739d8240fd086a
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-02 12:14:29 +0530

    Add files via upload

---
+++ condition5.java
@@ -0,0 +1,35 @@
+class sample
+ {
+    sample()
+	{
+	   int marks=75;
+
+	   if(marks<40)
+            {
+	      System.out.println("Re appear");
+	    }
+	   else if((marks>=40)&&(marks<60))
+	    {
+	      System.out.println("Pass");
+	    }
+	  else if((marks>60) &&(marks<80))
+	   {
+	      System.out.println("Agrade");
+	   }
+	 
+
+		}
+
+
+
+	}
+ 
+
+class condition5
+
+ {
+    public static void main(String asd[])
+	{
+	   sample obj=new sample();
+	}
+ }
