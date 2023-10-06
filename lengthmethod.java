7455f9c24ce0bd56d17ca480ed2755208c1f29b3
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-06 13:14:03 +0530

    Add files via upload

---
+++ lengthmethod.java
@@ -0,0 +1,11 @@
+public class lengthmethod
+{  
+    public static void main(String args[])
+    {  
+    String s1="HARIPRASANTH";  
+    String s2="TEKFILO";  
+    System.out.println("string length is: "+s1.length());
+    System.out.println("string length is: "+s2.length()); 
+    }
+}  
+    
