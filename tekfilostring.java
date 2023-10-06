171fb3dc6e07da001b9cadd8ac9415ad7154e213
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-06 13:08:44 +0530

    Add files via upload

---
+++ tekfilostring.java
@@ -0,0 +1,14 @@
+public class tekfilostring
+{  
+    public static void main(String args[])
+    {  
+    String s1="STRING";  
+    char ch[]={'T','E','K','F','I','L','O'};  
+    String s2=new String(ch);  
+    String s3=new String("BY HARI");  
+    System.out.println(s1);  
+    System.out.println(s2);  
+    System.out.println(s3);  
+    }
+}  
+   
