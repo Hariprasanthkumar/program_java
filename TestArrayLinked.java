a958b6105d5aeff6dc932f2e236b5ca2065be20b
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-10 15:40:43 +0530

    Add files via upload

---
+++ TestArrayLinked.java
@@ -0,0 +1,22 @@
+import java.util.*;    
+class TestArrayLinked
+{    
+ public static void main(String args[])
+ {    
+     
+  ArrayList<String> al=new ArrayList<String>();    
+  al.add("thalapathy");  
+  al.add("Vijay");    
+  al.add("leo");    
+  al.add("lokesh kanagaraj");    
+    
+ LinkedList<String> al2=new LinkedList<String>();    
+  al2.add("SuperStar"); 
+  al2.add("Rajini");    
+  al2.add("jailer");    
+  al2.add("Nelson");    
+    
+  System.out.println("Actor: "+al);  
+  System.out.println("Actor: "+al2);  
+ }    
+}    
