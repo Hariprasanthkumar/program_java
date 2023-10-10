bccf2734f9a6b4748f3e470cf927f111b75ea649
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-10 11:27:59 +0530

    Add files via upload

---
+++ ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+ public class ArrayList1
+ {  
+ public static void main(String args[])
+ {  
+  ArrayList<String> list=new ArrayList<String>();   
+      list.add("Hari");    
+      list.add("Praveen");    
+      list.add("Mohan");    
+      list.add("Suraj");    
+      
+      System.out.println(list);  
+ }  
+}  
