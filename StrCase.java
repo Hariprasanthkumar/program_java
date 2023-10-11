1a136e31ae06633c7f1cbe82573572dd9af136c7
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-11 12:06:39 +0530

    Add files via upload

---
+++ StrCase.java
@@ -0,0 +1,10 @@
+public class StrCase 
+{  
+public static void main(String ar[])  
+{  
+String s="Sachin";    
+System.out.println(s.toUpperCase());   
+System.out.println(s.toLowerCase());    
+System.out.println(s);   
+}  
+}  
