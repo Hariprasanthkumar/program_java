a39e21d23e4174c21f65790198beb619c1ab713c
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-12 15:14:56 +0530

    Add files via upload

---
+++ ststicimport.java
@@ -0,0 +1,11 @@
+import static java.lang.System.*;    
+class Staticimport
+{  
+  public static void main(String args[])
+  {  
+     
+   out.println("Hello");
+   out.println("Tekfilo");  
+  
+ }   
+}      
