fc6549bc7ab49052a924a62136e48e0d6eb03a5d
Hariprasanthkumar <145096810+Hariprasanthkumar@users.noreply.github.com>
2023-10-13 11:15:25 +0530

    Add files via upload

--- absolute.java
+++ absolute.java
@@ -1,12 +1,12 @@
-public class absolute 
+public class    absolute
 {  
     public static void main(String args[])  
     {  
-        int x = 78;  
-        int y = -48;  
-        //print the absolute value of int type  
+        long x = 78730343;  
+        long y = -4839233;  
+         
         System.out.println(Math.abs(x));  
         System.out.println(Math.abs(y));  
-        System.out.println(Math.abs(Integer.MIN_VALUE));  
+        System.out.println(Math.abs(Long.MIN_VALUE));  
     }  
 }  
